028103f3c88b9caacdafe996dbff3f6438353f12
amaresamkalyan <amaresamkalyan@gmail.com>
2017-01-08 22:34:41 -0400

    .

--- app/app.jsx
+++ app/app.jsx
@@ -3,6 +3,10 @@ var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 var Main = require('Main');
 
+// Load foundation
+require('style!css!foundation-sites/dist/css/foundation.min.css')
+$(document).foundation();
+
 
 // App css
 require('style!css!sass!applicationStyles')
