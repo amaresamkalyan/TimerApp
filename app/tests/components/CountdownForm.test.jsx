bacc610ead0ff0664b87adf090ab7c2e176390e7
amaresamkalyan <amaresamkalyan@gmail.com>
2017-01-10 02:11:25 -0400

    .

--- app/tests/components/CountdownForm.test.jsx
+++ app/tests/components/CountdownForm.test.jsx
@@ -29,32 +29,5 @@ describe('CountdownForm', () => {
 
         expect(spy).toNotHaveBeenCalled();
     });
-    // describe('render', () => {
-    //     it('Should render clock to o/p', () => {
-    //         var clock = TestUtils.renderIntoDocument(<Clock totalSeconds={62} />);
-    //         var $el = $(ReactDOM.findDOMNode(clock));
-    //         var actualText = $el.find('.clock-text').text();
-
-    //         expect(actualText).toBe('00:01:02');
-    //     });
-    // });
-    // describe('formatSeconds', () => {
-    //     it('Should formatSeconds', () => {
-    //         var clock = TestUtils.renderIntoDocument(<Clock />);
-    //         var seconds = 615;
-    //         var expected = '00:10:15';
-    //         var actual = clock.formatSeconds(seconds);
-
-    //         expect(actual).toBe(expected);
-    //     });
-    //     it('Should formatSeconds to 01:01', () => {
-    //         var clock = TestUtils.renderIntoDocument(<Clock />);
-    //         var seconds = 61;
-    //         var expected = '00:01:01';
-    //         var actual = clock.formatSeconds(seconds);
-
-    //         expect(actual).toBe(expected);
-    //     });
-    // });
 });
 
