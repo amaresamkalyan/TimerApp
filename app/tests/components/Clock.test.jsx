025b04b812f9977e48f4d3a7a136f35220b573b0
amaresamkalyan <amaresamkalyan@gmail.com>
2017-01-10 00:42:17 -0400

    .

---
+++ app/tests/components/Clock.test.jsx
@@ -0,0 +1,41 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var expect = require('expect');
+var $ = require('jQuery');
+var TestUtils = require('react-addons-test-utils');
+
+var Clock = require('Clock');
+
+describe('Clock', () => {
+    it('Should Exist', () => {
+        expect(Clock).toExist();
+    });
+    describe('render', () => {
+        it('Should render clock to o/p', () => {
+            var clock = TestUtils.renderIntoDocument(<Clock totalSeconds={62} />);
+            var $el = $(ReactDOM.findDOMNode(clock));
+            var actualText = $el.find('.clock-text').text();
+
+            expect(actualText).toBe('01:02');
+        });
+    });
+    describe('formatSeconds', () => {
+        it('Should formatSeconds', () => {
+            var clock = TestUtils.renderIntoDocument(<Clock />);
+            var seconds = 615;
+            var expected = '10:15';
+            var actual = clock.formatSeconds(seconds);
+
+            expect(actual).toBe(expected);
+        });
+        it('Should formatSeconds to 01:01', () => {
+            var clock = TestUtils.renderIntoDocument(<Clock />);
+            var seconds = 61;
+            var expected = '01:01';
+            var actual = clock.formatSeconds(seconds);
+
+            expect(actual).toBe(expected);
+        });
+    });
+});
+
